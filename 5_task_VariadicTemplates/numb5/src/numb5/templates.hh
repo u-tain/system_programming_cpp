870f176217982f798ca91512943e880a76c434c0
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-12 19:11:03 +0300

    Update templates.hh

--- 5_task_VariadicTemplates/numb5/src/numb5/templates.hh
+++ 5_task_VariadicTemplates/numb5/src/numb5/templates.hh
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespase std;
 
+template <class First, class... Other>
+void make_vector(First first, Other... other)
+{
+    
+  
+  cout << first;
+  print(other...);
+}
+
+template <class T, class ... Args>
+T make_vector(Args&& ... args) {
+return T(std::forward<Args>(args)...);
+}
+
 template<typename T, typename... Args>
 void message(ostream& streamOut, const char *s, T value, Args... args)
 {
+    make<vector<char>>(3, 'a'); // {'a', 'a', 'a'}
     i=0;
     while (*s) {
         if (*s == '%') {
