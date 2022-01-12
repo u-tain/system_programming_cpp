09441003ba3e2cd50d60b1354b96273abc3f5555
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-13 00:33:04 +0300

    Update templates.hh

--- 5_task_VariadicTemplates/numb5/src/numb5/templates.hh
+++ 5_task_VariadicTemplates/numb5/src/numb5/templates.hh
@@ -1,9 +1,11 @@
 #include <iostream>
-using namespase std;
+#include <string.h>
 
-template <class First>
+using namespace std;
+
+/*template <class First>
 void make_vector(vector<char>& x, First first) {
-return x.push_back(first);;
+return x.push_back(to_string(first));;
 }
 template <class First, class ... Other>
 void make_vector(vector<char>& x, First first, Other... other)
@@ -11,29 +13,29 @@ void make_vector(vector<char>& x, First first, Other... other)
     x.push_back(first);
     make_vector(x, other...);
 }
+*/
 
+inline void message() {}
+inline void message(ostream& streamOut) {} 
+template<typename T>
+void message(ostream& streamOut, const char *s, T value)
+{
+    streamOut<< value;
+}
 template<typename T, typename... Args>
 void message(ostream& streamOut, const char *s, T value, Args... args)
 {
-    vector<char> list;
-    make_vector(list, args...);
-    if list.size()<
-        
-    
-    i=0;
-    while (*s) {
-        if (*s == '%') {
-            i+=1;
-            if (*(s + 1) == '%') {
-                ++s;
-            }
-            else {
-                streamOut << value;
-                s += 2; // работает только для спецификаторов формата из двух символов (напр. %d, %f ).Не будет работать с %5.4f
-                message(s, args...); // вызов происходит даже когда *s == 0, чтобы обнаружить избыточные аргументы
-                return;
-            }
-        }
-        cout << *s++;
-    }    
+    while (*s!="%") {
+        if (*s == '\0')
+            return;
+            streamOut<<*s;
+    };
+        s++;
+        streamOut<< value;
+        message(streamOut, s,args...);
 }
+int main()
+{
+      message(cout, "% + % = %\n", 'a', 2, 3.0);
+      return 0;
+}           
