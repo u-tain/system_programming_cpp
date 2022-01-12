3dfca64613b7c105c6f2d48b4ace4800cdd90e22
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-12 06:37:44 +0300

    Update templates.hh

--- 5_task_VariadicTemplates/numb5/src/numb5/templates.hh
+++ 5_task_VariadicTemplates/numb5/src/numb5/templates.hh
@@ -2,7 +2,7 @@
 using namespase std;
 
 template<typename T, typename... Args>
-void message(const char *s, T value, Args... args)
+void message(ostream& streamOut, const char *s, T value, Args... args)
 {
     while (*s) {
         if (*s == '%') {
@@ -10,7 +10,7 @@ void message(const char *s, T value, Args... args)
                 ++s;
             }
             else {
-                cout << value;
+                streamOut << value;
                 s += 2; // работает только для спецификаторов формата из двух символов (напр. %d, %f ).Не будет работать с %5.4f
                 message(s, args...); // вызов происходит даже когда *s == 0, чтобы обнаружить избыточные аргументы
                 return;
