568a7fda9d37e46779d65703314e812c8dae1e5f
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-13 08:37:56 +0300

    Update cat.hh

--- 5_task_VariadicTemplates/numb5/src/numb5/cat.hh
+++ 5_task_VariadicTemplates/numb5/src/numb5/cat.hh
@@ -6,7 +6,7 @@ auto cat(array<T,N>& first, Array&...arrays)->array<T,N*sizeof...(aarays)+1>
 {
   T* C[sizeof ... (arrays)+1] = {first.data(), (arrays.data())...};
   array<T,N*(sizeof ...(arrays)+1)> concatenation;
-  for (int i=0; i<sizeof...(arrays)1;i++)
+  for (int i=0; i<sizeof...(arrays)+1;i++)
     for( int j = 0; j<N;j++)
       concatenation[i*N+j] = C[i][j];
   return concatenation;
