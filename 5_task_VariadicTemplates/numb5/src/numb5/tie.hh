61a6188e9ab45d9cf8d4539b27a251dc55e48e51
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-13 22:42:43 +0300

    Update tie.hh

--- 5_task_VariadicTemplates/numb5/src/numb5/tie.hh
+++ 5_task_VariadicTemplates/numb5/src/numb5/tie.hh
@@ -1,17 +1,29 @@
-
 #include <iostream>
+#include <string>
+#include<array>
+
 using namespace std;
-template<tipename First, tipename...Last>
+template<typename First, typename... Last>
   struct Tie{
     int N;
-  int M = sizeof...(Last)+1;
-  First* new_aray[sizeof...(Last)+1];
-  Tie(First& first, Last&...last){
-  N = first.size();
-  new_array[0] = &first;
-  First* array_last[sizeof..(Last)] = {&last ...};
-  for(int i =0; i<sizeof...(Last); i++)
-      new_array[i+1] = array_last[i];
-  }
- template<class T, long unsignet int K>
-  
+    int M = sizeof...(Last)+1;
+    First* new_array[sizeof...(Last)+1];
+    Tie(First& first, Last&...last){
+        N = first.size();
+        new_array[0] = &first;
+        First* array_last[sizeof...(Last)] = {&last ...};
+        for(int i =0; i<sizeof...(Last); i++)
+            new_array[i+1] = array_last[i];
+    }
+    template<class T, int K>
+    void operator=(const array<T,K>& rhs){
+        if (K==M*N)
+            for (int i = 0; i<M;i++)
+                for (int j = 0; j<N; j++)
+                    (*new_array[i])[j] = rhs[i*N+j];
+    }
+};
+template<typename First, typename... Last>
+auto tie(First& first, Last&... last)-> Tie<First, Last...>{
+    return Tie<First, Last...>(first, last...);
+}
