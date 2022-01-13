e066c7d0bac9f48ea1bead83824a2a8a2cc0f83c
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-13 09:24:49 +0300

    Update cat.hh

--- 5_task_VariadicTemplates/numb5/src/numb5/cat.hh
+++ 5_task_VariadicTemplates/numb5/src/numb5/cat.hh
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
 template<typename T, long unsigned N, typename...Array>
-auto cat(array<T,N>& first, Array&...arrays)->array<T,N*sizeof...(aarays)+1>
+auto cat( array<T,N>& first, Array&...arrays)->array<T,N*(sizeof...(arrays)+1)>
 {
   T* C[sizeof ... (arrays)+1] = {first.data(), (arrays.data())...};
   array<T,N*(sizeof ...(arrays)+1)> concatenation;
@@ -10,3 +11,4 @@ auto cat(array<T,N>& first, Array&...arrays)->array<T,N*sizeof...(aarays)+1>
     for( int j = 0; j<N;j++)
       concatenation[i*N+j] = C[i][j];
   return concatenation;
+}
