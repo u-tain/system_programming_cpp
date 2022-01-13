fd0e7ff2940b8deaf0d15f53be8d8f3c0b6bd796
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-13 05:22:42 +0300

    Update tie.hh

--- 5_task_VariadicTemplates/numb5/src/numb5/tie.hh
+++ 5_task_VariadicTemplates/numb5/src/numb5/tie.hh
@@ -1,7 +1,7 @@
 
 #include <iostream>
 using namespace std;
-template<template First, tipename...Last>
+template<tipename First, tipename...Last>
   struct Tie{
     int N;
   int M = sizeof...(Last)+1;
