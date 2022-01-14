83838d69fb0c7036a0a22ee814a03275e75a593d
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-14 04:55:14 +0300

    Create linalg.hh

---
+++ 6_task_LazyEvaluation/linalg/src/linalg/linalg.hh
@@ -0,0 +1,98 @@
+#ifndef LINALG_LINALG_HH
+#define LINALG_LINALG_HH
+
+#include <ostream>
+#include <vector>
+
+class Expression {};
+
+template <class T>
+class Vector;
+
+template <class E>
+Vector<typename E::value_type> evaluate(const E& expr) {
+    using value_type = typename E::value_type;
+    const auto n = expr.size();
+    Vector<value_type> result(n);
+    for (int i=0; i<n; ++i) {
+        result(i) = expr.evaluate(i);
+    }
+    return result;
+}
+
+template <class T>
+class Vector: public Expression {
+
+public:
+    using value_type = T;
+
+private:
+    std::vector<T> _data;
+
+public:
+
+    Vector(std::initializer_list<T> rhs): _data(rhs) {}
+    explicit Vector(int n): _data(n) {}
+    template <class E>
+    Vector(const E& expr,
+           typename std::enable_if<std::is_base_of<Expression,E>::value,E>::type* dummy=nullptr):
+    Vector(::evaluate(expr)) {}
+
+    Vector() = default;
+    ~Vector() = default;
+    Vector(Vector&& rhs) = default;
+    Vector(const Vector& rhs) = default;
+    Vector& operator=(Vector&& rhs) = default;
+    Vector& operator=(const Vector& rhs) = default;
+
+    T& operator()(int i) { return this->_data[i]; }
+    const T& operator()(int i) const { return this->_data[i]; }
+    T evaluate(int i) { return this->_data[i]; }
+    T evaluate(int i) const { return this->_data[i]; }
+    int size() const { return this->_data.size(); }
+    void display(std::ostream& out) const {
+        out << "Vector(";
+        const auto n = size();
+        if (n != 0) { out << this->_data.front(); }
+        for (int i=1; i<n; ++i) { out << ',' << this->_data[i]; }
+        out << ')';
+    }
+
+};
+
+template <class E>
+typename std::enable_if<std::is_base_of<Expression,E>::value,std::ostream&>::type
+operator<<(std::ostream& out, const E& expr) {
+    expr.display(out); return out;
+}
+
+template <class E1, class E2>
+class Plus: public Expression {
+
+public:
+    using value_type =
+        typename std::common_type<typename E1::value_type,typename E2::value_type>::type;
+
+private:
+    const E1& _a;
+    const E2& _b;
+
+public:
+    explicit Plus(const E1& a, const E2& b): _a(a), _b(b) {}
+    value_type evaluate(int i) { return this->_a.evaluate(i) + this->_b.evaluate(i); }
+    value_type evaluate(int i) const { return this->_a.evaluate(i) + this->_b.evaluate(i); }
+    int size() const { return this->_a.size(); }
+    void display(std::ostream& out) const {
+        out << "Plus(" << this->_a << ", " << this->_b << ')';
+    }
+
+};
+
+template <class E1, class E2>
+typename std::enable_if<std::is_base_of<Expression,E1>::value &&
+                        std::is_base_of<Expression,E2>::value,Plus<E1,E2>>::type
+operator+(const E1& a, const E2& b) {
+    return Plus<E1,E2>(a,b);
+}
+
+#endif // vim:filetype=cpp
