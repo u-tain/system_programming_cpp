8f5e4a267f6ec3b5409e59bfde92f8dd99fa04ad
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-14 22:54:09 +0300

    Update cat.hh

--- 5_task_VariadicTemplates/numb5/src/numb5/cat.hh
+++ 5_task_VariadicTemplates/numb5/src/numb5/cat.hh
@@ -2,7 +2,7 @@
 #include <array>
 using namespace std;
 
-template<typename T, long unsigned N, typename...Array>
+/*template<typename T, long unsigned N, typename...Array>
 auto cat( array<T,N>& first, Array&...arrays)->array<T,N*(sizeof...(arrays)+1)>
 {
   T* C[sizeof ... (arrays)+1] = {first.data(), (arrays.data())...};
@@ -11,4 +11,22 @@ auto cat( array<T,N>& first, Array&...arrays)->array<T,N*(sizeof...(arrays)+1)>
     for( int j = 0; j<N;j++)
       concatenation[i*N+j] = C[i][j];
   return concatenation;
+}*/
+
+template<typename T, long unsigned N>
+auto cat(array < T, N>& value){
+    return value;
+}
+template<typename T, long unsigned N, long unsigned M, typename... Args>
+auto cat(array < T, N>& first, array < T, M>& second, Args&... args){
+    array<T, N + M> x;
+    for (size_t i = 0; i < N + M; i++) {
+        if (i < N) {
+            x[i] = first[i];
+        }
+        else {
+            x[i] = second[i - N];
+        }
+     }
+    return cat(x, args...);
 }
