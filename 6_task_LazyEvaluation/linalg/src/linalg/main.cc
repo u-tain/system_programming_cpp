a82492ae4dbb588bb9fe4a0401923e07551a8100
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-14 04:57:11 +0300

    Create main.cc

---
+++ 6_task_LazyEvaluation/linalg/src/linalg/main.cc
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+#include <linalg/linalg.hh>
+
+int main(int argc, char* argv[]) {
+    using T = float;
+    Vector<T> a{1,2,3}, b{4,5,6}, c{7,8,9};
+    Vector<T> d(a+b+c);
+    std::cout << (a+b+c) << std::endl;
+    std::cout << evaluate(a+b+c) << std::endl;
+    std::cout << d << std::endl;
+    return 0;
+}
