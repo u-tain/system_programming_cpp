ba99370611bdb1e9e26ea5b55c1fd048abc34218
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-15 18:07:23 +0300

    Update lazy_test.cc

--- 6_task_LazyEvaluation/linalg/src/test/lazy_test.cc
+++ 6_task_LazyEvaluation/linalg/src/test/lazy_test.cc
@@ -15,11 +15,40 @@ TEST(lazy_evaluatuion, unary_minus){
 }
 
 TEST(lazy_evaluatuion, sravnenie) {
-        vector<float> x;
-        vector<float> y = {2,1};
-        vector<float> z = {2};
-        x.push_back(2);
-        EXPECT_EQ(z, x);
-        x.push_back(1);
-        EXPECT_EQ(y, x);
+        using T = int;
+        Vector<float> a{ 1, 2, 3 }, b{ 4,5,6 }, c{ 7,8,9 }, e{1,1,1}, v{0,0,0};
+        Vector<T> d = a<b;
+        EXPECT_EQ(e, d);
+    
+        Vector<T> n = c<b;
+        EXPECT_EQ(v, n);
+    
+        Vector<T> m = a<a;
+        EXPECT_EQ(v, m);
+}
+
+TEST(lazy_evaluatuion, Bin_minus) {
+        using T = float;
+        Vector<T> a{ 1, 2, 3 }, b{ 4,5,6 }, c{ 7,8,9 }, e{3,3,3}, v{0,0,0}, q{-3,-3,-3};
+        Vector<T> d = a-b;
+        EXPECT_EQ(q, d);
+    
+        Vector<T> n = c-b;
+        EXPECT_EQ(e, n);
+    
+        Vector<T> m = a-a;
+        EXPECT_EQ(v, m);
+}
+
+TEST(lazy_evaluatuion, ternarm_oper) {
+        using T = float;
+        Vector<T> a{ 1, 2, 3 }, b{ 4,5,6 }, c{ 7,8,9 };
+        Vector<T> d(where(a < b, b, c));
+        EXPECT_EQ(b, d);
+    
+        Vector<T> e(where(c < b, b, c));
+        EXPECT_EQ(e, c);
+    
+        Vector<T> n(where(c < c, b, c));
+        EXPECT_EQ(n, c);
 }
