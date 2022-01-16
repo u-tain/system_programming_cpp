caee7b1d4aeed9ef3d67ede58d97dc389edcfd00
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-16 05:30:03 +0300

    Update lazy_test.cc

--- 6_task_LazyEvaluation/linalg/src/test/lazy_test.cc
+++ 6_task_LazyEvaluation/linalg/src/test/lazy_test.cc
@@ -5,50 +5,33 @@
 using namespace std;
 
 TEST(lazy_evaluatuion, unary_minus){
-    using T = float;
-    Vector<T> a{ 1, 2, 3 }, b{0, 0}, c{-1,-2,-3};
-    Vector<T> d = -a;
-    EXPECT_EQ(c, d);
-    
-    Vector<T> e = -b;
-    EXPECT_EQ(b, e);
+        using T = float;
+        Vector<T> a{ 1, 2, 3 }, b{0, 0}, c{-1,-2,-3};
+        EXPECT_EQ(c, -a);
+        EXPECT_EQ(b, -b);
 }
 
 TEST(lazy_evaluatuion, sravnenie) {
-        using T = int;
-        Vector<float> a{ 1, 2, 3 }, b{ 4,5,6 }, c{ 7,8,9 }, e{1,1,1}, v{0,0,0};
-        Vector<T> d = a<b;
-        EXPECT_EQ(e, d);
-    
-        Vector<T> n = c<b;
-        EXPECT_EQ(v, n);
-    
-        Vector<T> m = a<a;
-        EXPECT_EQ(v, m);
+        Vector<float> a{ 1, 2, 3 }, b{ 4,5,6 }, c{ 7,8,9 }, d{1,12,3};
+        Vector<int> e{1,1,1}, v{0,0,0}, x{1,0,1};
+        EXPECT_EQ(e, a<b);
+        EXPECT_EQ(v, c<b);
+        EXPECT_EQ(v, a<a);
+        EXPECT_EQ(x, d<b);                                                                                     
 }
 
 TEST(lazy_evaluatuion, Bin_minus) {
         using T = float;
         Vector<T> a{ 1, 2, 3 }, b{ 4,5,6 }, c{ 7,8,9 }, e{3,3,3}, v{0,0,0}, q{-3,-3,-3};
-        Vector<T> d = a-b;
-        EXPECT_EQ(q, d);
-    
-        Vector<T> n = c-b;
-        EXPECT_EQ(e, n);
-    
-        Vector<T> m = a-a;
-        EXPECT_EQ(v, m);
+        EXPECT_EQ(q, a-b);
+        EXPECT_EQ(e, c-b);
+        EXPECT_EQ(v, a-a);
 }
 
 TEST(lazy_evaluatuion, ternarm_oper) {
         using T = float;
         Vector<T> a{ 1, 2, 3 }, b{ 4,5,6 }, c{ 7,8,9 };
-        Vector<T> d(where(a < b, b, c));
-        EXPECT_EQ(b, d);
-    
-        Vector<T> e(where(c < b, b, c));
-        EXPECT_EQ(e, c);
-    
-        Vector<T> n(where(c < c, b, c));
-        EXPECT_EQ(n, c);
+        EXPECT_EQ(where(a < b, b, c), b);
+        EXPECT_EQ(where(c < b, b, c), c);
+        EXPECT_EQ(where(c < c, b, c), c);
 }
