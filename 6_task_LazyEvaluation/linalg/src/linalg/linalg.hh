9ca25303f402a39138725705a3f50dc6b6c4a103
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-17 00:28:42 +0300

    Update linalg.hh

--- 6_task_LazyEvaluation/linalg/src/linalg/linalg.hh
+++ 6_task_LazyEvaluation/linalg/src/linalg/linalg.hh
@@ -8,7 +8,7 @@ class Expression {};
 
 template <class T>
 class Vector;
-/// метод для определения размера массива
+
 template <class E>
 Vector<typename E::value_type> evaluate(const E& expr) {
     using value_type = typename E::value_type;
@@ -16,6 +16,19 @@ Vector<typename E::value_type> evaluate(const E& expr) {
     Vector<value_type> result(n);
     for (int i=0; i<n; ++i) {
         result(i) = expr.evaluate(i);
+    }
+    return result;
+}
+template <class E>
+bool evaluate1(const E& expr) {
+    using value_type = typename E::value_type;
+    const auto n = expr.size();
+    bool result = true;
+    for (int i = 0; i < n; ++i) {
+        if (result == expr.evaluate(i))
+            result = true;
+        else
+            result = false;
     }
     return result;
 }
@@ -45,6 +58,18 @@ public:
     Vector& operator=(Vector&& rhs) = default;
     Vector& operator=(const Vector& rhs) = default;
 
+    bool operator!=(const Vector& rhs) const {
+        const auto n = size();
+        if (n != rhs.size())
+            return true;
+        for (int i = 0; i < n; ++i) {
+            if (this->_data[i] != rhs._data[i])
+                return true;
+        }
+        return false;
+    }
+    bool operator==(const Vector& rhs) const {return !(*this != rhs);}
+
     T& operator()(int i) { return this->_data[i]; }
     const T& operator()(int i) const { return this->_data[i]; }
     T evaluate(int i) { return this->_data[i]; }
@@ -59,13 +84,13 @@ public:
     }
 
 };
-/// оператор вывода
+// реализация бинарного +
 template <class E>
 typename std::enable_if<std::is_base_of<Expression,E>::value,std::ostream&>::type
 operator<<(std::ostream& out, const E& expr) {
     expr.display(out); return out;
 }
-/// Реализация операции Plus
+
 template <class E1, class E2>
 class Plus: public Expression {
 
@@ -87,7 +112,7 @@ public:
     }
 
 };
-/// оператор сложения
+
 template <class E1, class E2>
 typename std::enable_if<std::is_base_of<Expression,E1>::value &&
                         std::is_base_of<Expression,E2>::value,Plus<E1,E2>>::type
@@ -97,19 +122,20 @@ operator+(const E1& a, const E2& b) {
 
 /// реализация унарного -
 template <class E>
-class M: public Expression {
+class M : public Expression {
 public:
     using value_type =
         typename std::common_type<typename E::value_type>::type;
 private:
-    const E& _a;  
+    const E& _a;
 public:
-    explicit M(const E& a): _a(a) {}
+    explicit M(const E& a) : _a(a) {}
     value_type evaluate(int i) {
         if (this->_a.evaluate(i) == 0)
             return this->_a.evaluate(i);
-        else 
-            return - this-> _a.evaluate(i); }
+        else
+            return -this->_a.evaluate(i);
+    }
     value_type evaluate(int i) const {
         if (this->_a.evaluate(i) == 0)
             return this->_a.evaluate(i);
@@ -122,46 +148,46 @@ public:
     }
 };
 template <class E>
-typename std::enable_if<std::is_base_of<Expression,E>::value ,M<E>>::type
+typename std::enable_if<std::is_base_of<Expression, E>::value, M<E>>::type
 operator-(const E& a) {
     return M<E>(a);
 }
 /// реализация унарного +
 template <class E>
-class P: public Expression {
+class P : public Expression {
 public:
     using value_type =
         typename std::common_type<typename E::value_type>::type;
 private:
-    const E& _a;  
+    const E& _a;
 public:
-    explicit P(const E& a): _a(a) {}
-    value_type evaluate(int i) { return  this-> _a.evaluate(i); }
-    value_type evaluate(int i) const { return  this-> _a.evaluate(i); }
+    explicit P(const E& a) : _a(a) {}
+    value_type evaluate(int i) { return  this->_a.evaluate(i); }
+    value_type evaluate(int i) const { return  this->_a.evaluate(i); }
     int size() const { return this->_a.size(); }
     void display(std::ostream& out) const {
         out << "P(" << this->_a << ')';
     }
 };
 template <class E>
-typename std::enable_if<std::is_base_of<Expression,E>::value ,P<E>>::type
+typename std::enable_if<std::is_base_of<Expression, E>::value, P<E>>::type
 operator+(const E& a) {
     return P<E>(a);
 }
 /// реализация бинарного -
 template <class E1, class E2>
-class Minus: public Expression {
+class Minus : public Expression {
 
 public:
     using value_type =
-        typename std::common_type<typename E1::value_type,typename E2::value_type>::type;
+        typename std::common_type<typename E1::value_type, typename E2::value_type>::type;
 
 private:
     const E1& _a;
     const E2& _b;
 
 public:
-    explicit Minus(const E1& a, const E2& b): _a(a), _b(b) {}
+    explicit Minus(const E1& a, const E2& b) : _a(a), _b(b) {}
     value_type evaluate(int i) { return this->_a.evaluate(i) - this->_b.evaluate(i); }
     value_type evaluate(int i) const { return this->_a.evaluate(i) - this->_b.evaluate(i); }
     int size() const { return this->_a.size(); }
@@ -172,25 +198,25 @@ public:
 };
 
 template <class E1, class E2>
-typename std::enable_if<std::is_base_of<Expression,E1>::value &&
-                        std::is_base_of<Expression,E2>::value,Minus<E1,E2>>::type
-operator-(const E1& a, const E2& b) {
-    return Minus<E1,E2>(a,b);
+typename std::enable_if<std::is_base_of<Expression, E1>::value&&
+    std::is_base_of<Expression, E2>::value, Minus<E1, E2>>::type
+    operator-(const E1& a, const E2& b) {
+    return Minus<E1, E2>(a, b);
 }
 /// реализация бинарного оператора *
 template <class E1, class E2>
-class Proizv: public Expression {
+class Proizv : public Expression {
 
 public:
     using value_type =
-        typename std::common_type<typename E1::value_type,typename E2::value_type>::type;
+        typename std::common_type<typename E1::value_type, typename E2::value_type>::type;
 
 private:
     const E1& _a;
     const E2& _b;
 
 public:
-    explicit Proizv(const E1& a, const E2& b): _a(a), _b(b) {}
+    explicit Proizv(const E1& a, const E2& b) : _a(a), _b(b) {}
     value_type evaluate(int i) { return this->_a.evaluate(i) * this->_b.evaluate(i); }
     value_type evaluate(int i) const { return this->_a.evaluate(i) * this->_b.evaluate(i); }
     int size() const { return this->_a.size(); }
@@ -201,26 +227,26 @@ public:
 };
 
 template <class E1, class E2>
-typename std::enable_if<std::is_base_of<Expression,E1>::value &&
-                        std::is_base_of<Expression,E2>::value,Proizv<E1,E2>>::type
-operator*(const E1& a, const E2& b) {
-    return Proizv<E1,E2>(a,b);
+typename std::enable_if<std::is_base_of<Expression, E1>::value&&
+    std::is_base_of<Expression, E2>::value, Proizv<E1, E2>>::type
+    operator*(const E1& a, const E2& b) {
+    return Proizv<E1, E2>(a, b);
 }
 /// реализация бинарного оператора /
 template <class E1, class E2>
-class Del: public Expression {
+class Del : public Expression {
 
 public:
     using value_type =
-        typename std::common_type<typename E1::value_type,typename E2::value_type>::type;
+        typename std::common_type<typename E1::value_type, typename E2::value_type>::type;
 
 private:
     const E1& _a;
     const E2& _b;
 
 public:
-    explicit Del(const E1& a, const E2& b): _a(a), _b(b) {}
-    value_type evaluate(int i) {return this->_a.evaluate(i) / this->_b.evaluate(i); }
+    explicit Del(const E1& a, const E2& b) : _a(a), _b(b) {}
+    value_type evaluate(int i) { return this->_a.evaluate(i) / this->_b.evaluate(i); }
     value_type evaluate(int i) const { return this->_a.evaluate(i) / this->_b.evaluate(i); }
     int size() const { return this->_a.size(); }
     void display(std::ostream& out) const {
@@ -230,14 +256,14 @@ public:
 };
 
 template <class E1, class E2>
-typename std::enable_if<std::is_base_of<Expression,E1>::value &&
-                        std::is_base_of<Expression,E2>::value,Del<E1,E2>>::type
-operator/(const E1& a, const E2& b) {
-    return Del<E1,E2>(a,b);
+typename std::enable_if<std::is_base_of<Expression, E1>::value&&
+    std::is_base_of<Expression, E2>::value, Del<E1, E2>>::type
+    operator/(const E1& a, const E2& b) {
+    return Del<E1, E2>(a, b);
 }
-/// реализация операратора >
+/// реализация операратора <
 template <class E1, class E2>
-class Sravn1: public Expression {
+class Sravn : public Expression {
 
 public:
     using value_type = int;
@@ -247,20 +273,78 @@ private:
     const E2& _b;
 
 public:
-    explicit Sravn1(const E1& a, const E2& b): _a(a), _b(b) {}
-    value_type evaluate(int i) {return this->_a.evaluate(i) > this->_b.evaluate(i);}
-    value_type evaluate(int i) const {return this->_a.evaluate(i) > this->_b.evaluate(i);}
+    explicit Sravn(const E1& a, const E2& b) : _a(a), _b(b) {}
+    value_type evaluate(int i) { return this->_a.evaluate(i) < this->_b.evaluate(i); }
+    value_type evaluate(int i) const { return this->_a.evaluate(i) < this->_b.evaluate(i); }
     int size() const { return this->_a.size(); }
     void display(std::ostream& out) const {
-        out << "Sravn1(" << this->_a << ", " << this->_b << ')';
+        out << "Sravn(" << this->_a << ", " << this->_b << ')';
     }
 
 };
 
 template <class E1, class E2>
-typename std::enable_if<std::is_base_of<Expression,E1>::value &&
-                        std::is_base_of<Expression,E2>::value,Sravn1<E1,E2>>::type
-operator>(const E1& a, const E2& b) {
-    return Sravn1<E1,E2>(a,b);
+typename std::enable_if<std::is_base_of<Expression, E1>::value&&
+    std::is_base_of<Expression, E2>::value, Sravn<E1, E2>>::type
+    operator<(const E1& a, const E2& b) {
+    return Sravn<E1, E2>(a, b);
+}
+/// реализация операратора all
+template <class E>
+class All : public Expression {
+
+public:
+    using value_type = bool;
+
+private:
+    const E& _a;
+
+public:
+    explicit All(const E& a) : _a(a) {}
+    value_type evaluate(int i) { return this->_a.evaluate1(i); }
+    value_type evaluate(int i) const { return this-> _a.evaluate1(i); }
+    int size() const { return this->_a.size(); }
+    void display(std::ostream& out) const {
+        out << "All(" << this->_a << ')';
+    }
+
+};
+
+template <class E>
+auto all(const E& a) {
+    return All<E>(a);;
+}
+
+
+/// реализация ?:
+template <class E1, class E2, class E3>
+class Where : public Expression {
+
+public:
+    using value_type =
+        typename std::common_type<typename E1::value_type, typename E2::value_type, typename E3::value_type> ::type;
+
+
+private:
+    const E1& _a;
+    const E2& _b;
+    const E3& _c;
+
+public:
+    explicit Where(const E1& a, const E2& b, const E3& c) : _a(a), _b(b), _c(c) {}
+    value_type evaluate(int i) { return this->_a.evaluate(i) ? this->_b.evaluate(i) : this->_c.evaluate(i); }
+    value_type evaluate(int i) const { return this->_a.evaluate(i) ? this->_b.evaluate(i): this->_c.evaluate(i); }
+    int size() const { return this->_a.size(); }
+    void display(std::ostream& out) const {
+        out << "Where(" << this->_a << ", " << this->_b << ')';
+    }
+
+};
+
+template <class E1, class E2, class E3>
+typename std::enable_if<std::is_base_of<Expression, E1>::value&&
+    std::is_base_of<Expression, E2>::value&& std::is_base_of<Expression, E3>::value, Where<E1, E2, E3>>::type
+    where(const E1& a, const E2& b, const E3& c) {
+    return Where<E1, E2, E3>(a, b,c);
 }
 #endif // vim:filetype=cpp
