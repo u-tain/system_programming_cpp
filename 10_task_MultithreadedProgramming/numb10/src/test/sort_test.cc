95204345ffb718e12c1a4a5d755a1c7aecbbcc7f
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-16 06:59:07 +0300

    Update sort_test.cc

--- 10_task_MultithreadedProgramming/numb10/src/test/sort_test.cc
+++ 10_task_MultithreadedProgramming/numb10/src/test/sort_test.cc
@@ -1,3 +1,10 @@
 #include <gtest/gtest.h>
 
 using namespace std;
+TEST(merge, sort) {
+        vector<float> x;
+        merge_sort_multithread(arr2, buffer, threads_count);
+        EXPECT_EQ(0u, x.size());
+        x.push_back(1);
+        EXPECT_EQ(1u, x.size());
+}
