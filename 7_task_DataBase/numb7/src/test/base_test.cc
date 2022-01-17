5afe9015d19e6263cb9c03dbf13eb6a4aee69afc
Татьяна <43996253+u-tain@users.noreply.github.com>
2022-01-17 08:02:23 +0300

    Create base_test.cc

---
+++ 7_task_DataBase/numb7/src/test/base_test.cc
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <numb7/database.hh>
+
+using namespace std;
+
+TEST(database,first){}
+TEST(database,second){}
+TEST(database,third){}
+TEST(database,fourth){}
